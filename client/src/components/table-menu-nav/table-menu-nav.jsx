1ea5b509b2544f4520c954d599cc91113cbcb896
dm25an22 <dm25an22@gmail.com>
2021-01-19 14:44:27 +0100

    add fuse.js in dependencies

--- client/src/components/table-menu-nav/table-menu-nav.jsx
+++ client/src/components/table-menu-nav/table-menu-nav.jsx
@@ -3,6 +3,7 @@ import { useSelector } from "react-redux";
 import { MenuContext } from "../../contexts/menu-provider";
 import MenuCard from "../menu-card/menu-card";
 import { getProducts } from "../../redux-store/menu/selector";
+import Fuse from "fuse.js";
 import style from "./table-menu-nav.module.css";
 
 export default function TableMenuNav() {
@@ -10,17 +11,19 @@ export default function TableMenuNav() {
     MenuContext
   );
   const products = useSelector(getProducts);
+  const fuse = new Fuse(products, {
+    keys: ["name"]
+  });
+  const resultFuseSearch = fuse.search(searchValue);
 
   return (
     <nav ref={navRef} className={style.menuNav}>
       <ul className={style.menuNavList}>
         {isActiveSearch ? (
           <Fragment>
-            {products
-              .filter((it) => searchValue === it.name)
-              .map((item) => {
-                return <MenuCard key={item.name} item={item} />;
-              })}
+            {resultFuseSearch.map((product) => {
+              return <MenuCard key={product.item.name} item={product.item} />;
+            })}
           </Fragment>
         ) : (
           <Fragment>
