4d71fbbbcbb70b5151fac6b01daab5d63589f9e2
dm25an22 <dm25an22@gmail.com>
2021-01-20 02:39:49 +0100

    refactoring client api

--- client/src/api.js
+++ client/src/api.js
@@ -1,8 +1,13 @@
 export default class Api {
-  static async fetchMenu() {
-    const response = await fetch(`http://localhost:5000/api/menu/menu`);
-    const result = await response.json();
+  static _endPoint = `http://localhost:5000`;
 
+  static async _apiGET(url) {
+    const response = await fetch(`${this._endPoint}${url}`);
+    const result = await response.json();
     return result;
+  }
+
+  static async fetchMenu() {
+    return await this._apiGET(`/api/menu/menu`);
   }
 }
