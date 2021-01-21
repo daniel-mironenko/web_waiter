0b090b197257a2d7549d1b5552216028dfb1c566
dm25an22 <dm25an22@gmail.com>
2021-01-22 00:21:30 +0100

    create session storage

--- client/src/redux-store/user/user-reducer.js
+++ client/src/redux-store/user/user-reducer.js
@@ -1,10 +1,18 @@
 import Api from "../../api";
 import { AuthorizationStatus } from "../../enums";
+import { loadState, saveState } from "../../session-storage";
 
 const initialState = {
   userData: null,
   authorizationStatus: AuthorizationStatus.NO_AUTH
 };
+
+let localState
+try {
+  localState = { ...initialState, ...loadState() }
+} catch (e) {
+  localState = initialState
+}
 
 const ActionType = {
   LOGIN_SUCCESS: "LOGIN_SUCCESS",
@@ -21,10 +29,11 @@ const ActionCreator = {
 
 export const Operation = {
   login(data, onSuccess, onError) {
-    return async (dispatch) => {
+    return async (dispatch, getState) => {
       try {
         const userInfo = await Api.loginUser(data);
         dispatch(ActionCreator.loginSuccess(userInfo));
+        saveState(getState().USER);
         onSuccess();
       } catch (e) {
         onError();
@@ -33,7 +42,7 @@ export const Operation = {
   }
 }
 
-export function reducer(state = initialState, action) {
+export function reducer(state = localState, action) {
   switch (action.type) {
     case ActionType.LOGIN_SUCCESS:
       return { ...state, userData: action.payload, authorizationStatus: AuthorizationStatus.AUTH };
