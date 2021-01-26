a7df50467440eccbf732461f6452a64900f217fa
dm25an22 <dm25an22@gmail.com>
2021-01-26 11:15:27 +0100

    delete btn

--- client/src/components/buttons/danger-button/danger-button.jsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import style from "./danger-button.module.css";
-
-export default function DangerButton({ text }) {
-  return <button className={style.btnDanger}>{text}</button>;
-}
