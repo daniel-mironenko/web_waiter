ecabaef1d37c8896482f32e41e7e65fb0c41c8af
dm25an22 <dm25an22@gmail.com>
2021-01-30 20:25:48 +0100

    update index.js

--- client/src/App.js
+++ client/src/App.js
@@ -5,51 +5,18 @@ import LoginPage from "./pages/login-page/login-page";
 import ErrorPage from "./pages/error-page/error-page";
 import PrivateRoute from "./components/private-route/private-route";
 import OrderPage from "./pages/order-page/order-page";
-import { useDispatch, useSelector } from "react-redux";
-import { useEffect } from "react";
-import { getAuthStatus, getUserData } from "./redux-store/user/selector";
-import { Operation as OrdersOperation } from "./redux-store/orders/orders-reducer";
-import PageLoader from "./components/page-loader/page-loader";
-import { useLoadStatus } from "./hooks"
 
 export default function App() {
-  const { isLoaded, setIsLoaded, error, setError } = useLoadStatus();
-  const authStatus = useSelector(getAuthStatus);
-  const user = useSelector(getUserData);
-  const dispatch = useDispatch();
-
-  function onSuccessLoad() {
-    setIsLoaded(true);
-  }
-
-  function onErrorLoad(error) {
-    setError(error);
-  }
-
-  useEffect(() => {
-    if (authStatus) {
-      dispatch(OrdersOperation.loadActiveOrders(user.id, onSuccessLoad, onErrorLoad));
-    }
-  }, [])
-
-  if (error) {
-    return <ErrorPage message={error} />
-  } else if (!isLoaded) {
-    return (
-      <PageLoader />
-    )
-  } else {
-    return (
-      <BrowserRouter>
-        <Switch>
-          <PrivateRoute exact path={appRoute.PRIVAT_OFFICE} component={PrivatOfficePage} />
-          <Route exact path={`${appRoute.LOGIN}`} component={LoginPage} />
-          <PrivateRoute exact path={`${appRoute.ORDER}/:id`} component={OrderPage} />
-          <Route>
-            <ErrorPage message={"Страница не найдена"} />
-          </Route>
-        </Switch>
-      </BrowserRouter>
-    );
-  }
+  return (
+    <BrowserRouter>
+      <Switch>
+        <PrivateRoute exact path={appRoute.PRIVAT_OFFICE} component={PrivatOfficePage} />
+        <Route exact path={`${appRoute.LOGIN}`} component={LoginPage} />
+        <PrivateRoute exact path={`${appRoute.ORDER}/:id`} component={OrderPage} />
+        <Route>
+          <ErrorPage message={"Страница не найдена"} />
+        </Route>
+      </Switch>
+    </BrowserRouter>
+  );
 };
--- client/src/hooks.js
+++ client/src/hooks.js
@@ -2,7 +2,7 @@ import { useEffect, useRef, useState } from "react";
 
 export const useLoadStatus = () => {
   const [error, setError] = useState(null);
-  const [isLoaded, setIsLoaded] = useState(false);
+  const [isLoaded, setIsLoaded] = useState(null);
 
   return {
     isLoaded,
